8d75d407415bd556374eac608211267292b97dd2
Ravda-boeing <boeing747windows7@gmail.com>
2026-08-22 16:40:09 +1000

    Update boot.js

--- dist/boot.js
+++ dist/boot.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 // Redirect after fade-out
                 setTimeout(() => {
-                    window.location.href = "lockscreen.html";
+                    window.location.href = "https://ravda-boeing.github.io/lockscreen.html";
                 }, 2000);
 
             }, 9000);
